9b69cfb50b80bdcbef707dcd3993c159191950db
Impapon <paponsarker1@gmail.com>
2021-10-05 02:12:41 +0600

    just api id added

--- src/components/main/ApiResult.js
+++ src/components/main/ApiResult.js
@@ -1,1 +1,18 @@
 // this will return the api call result as json file
+
+import React from 'react';
+import './ApiResult.css'
+
+const ApiResult = (props) => {
+    console.log(props)
+    const {temp,humidity}=props.weather
+    return (
+        <div  className="result">
+            <h1>hi AMi result dekhabo</h1>
+            <h1> {temp}  </h1>
+            <h1> {humidity}  </h1>
+        </div>
+    );
+};
+
+export default ApiResult;
--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import './Main.css'
+import ApiResult from './ApiResult'
 
 function Main() {
+    const [city, setCity] = useState('')
+    const [weatherInfo, setWeatherInfo] = useState([])
+
+    /**
+     * after clicking the btn "handleWeatherBtn" will call & load weather data
+     * here dynamic city found by user given input  
+     */
+    function handleWeatherBtn() {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=841218244979a82d9ada69c23d770258&units=metric`)
+            .then(res => res.json())
+            .then(data => setWeatherInfo(data.main))
+    }
 
     return (
         <main>
             <div className='search-container'>
-                <input
+                <input onChange={(e) => setCity(e.target.value)}
                     type="text"
                     placeholder='Enter city name'
                 />
-                <button>Enter</button>
+                <button onClick={handleWeatherBtn}>Enter</button>
             </div>
+            {
+             
+                    <ApiResult key={weatherInfo.id} weather={weatherInfo}/>
+            }
+
         </main>
     );
 }
